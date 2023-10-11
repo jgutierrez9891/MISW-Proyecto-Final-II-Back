7d76266b5303aa7b66b4bb5e636c7f76a4bc0674
jgutierrez9891 <j.gutierrezl@uniandes.edu.co>
2023-10-10 21:22:53 -0500

    Add gitignore

--- candidate/src/app.py
+++ candidate/src/app.py
@@ -20,5 +20,5 @@ api = Api(app)
 api.add_resource(VistaCrearCandidato, '/candidato/create')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(debug=True, host='0.0.0.0', port=port)
