5d355d0ce0b75bcc42a82c2d5e45bea436881253
Jorge Enrique Suárez González <je.suarezg1@uniandes.edu.co>
2023-10-20 18:28:19 -0500

    Finaliza implementación 94% cobertura

--- empresa/database/database.sql
+++ empresa/database/database.sql
@@ -7,9 +7,9 @@ CREATE TABLE empresa(
     id int not null AUTO_INCREMENT,
     tipo_doc varchar(10) NOT NULL,
     num_doc varchar(50) NOT NULL,
-    nombre varchar(50) NOT NULL,
-    direccion varchar(100) NOT NULL,
-    telefono varchar(50) NOT NULL,
+    nombre varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    telefono varchar(30) NOT NULL,
     PRIMARY KEY(id)
 );
 
@@ -17,8 +17,10 @@ CREATE TABLE representante(
     id int not null AUTO_INCREMENT,
     tipo_doc varchar(10) NOT NULL,
     num_doc varchar(50) NOT NULL,
-    nombre varchar(50) NOT NULL,
-    direccion varchar(100) NOT NULL,
-    telefono varchar(50) NOT NULL,
+    nombre varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    telefono varchar(30) NOT NULL,
+    usuario varchar(50) NOT NULL,
+    clave varchar(50) NOT NULL,
     PRIMARY KEY(id)
 );
--- empresa/src/modelos/modelos.py
+++ empresa/src/modelos/modelos.py
@@ -6,23 +6,32 @@ from flask_marshmallow import Marshmallow
 db = SQLAlchemy()
 ma = Marshmallow()
 
-class Candidato(db.Model):
+class Empresa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tipo_doc = db.Column(db.String(50))
+    tipo_doc = db.Column(db.String(10))
     num_doc = db.Column(db.String(50))
     nombre = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    telefono = db.Column(db.String(20))
+
+class EmpresaSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Empresa
+        include_relationships = False
+        load_instance = True
+    
+class Representante(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tipo_doc = db.Column(db.String(10))
+    num_doc = db.Column(db.String(50))
+    nombre = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    telefono = db.Column(db.String(20))
     usuario = db.Column(db.String(50))
     clave = db.Column(db.String(50))
-    telefono = db.Column(db.Integer)
-    email = db.Column(db.String(100))
-    pais = db.Column(db.String(50))
-    ciudad = db.Column(db.String(50))
-    aspiracion_salarial = db.Column(db.Integer)
-    fecha_nacimiento = db.Column(db.DateTime)
-    idiomas = db.Column(db.String(200))
 
-class CandidatoSchema(ma.SQLAlchemyAutoSchema):
+class RepresentanteSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = Candidato
+        model = Empresa
         include_relationships = False
         load_instance = True
