490a91393a99d1ea168c578f8c4abdef14b25912
CharlyMoraUA <ca.morar1@uniandes.edu.co>
2023-10-22 18:29:13 -0500

    TEST

--- autenticacion/src/app.py
+++ autenticacion/src/app.py
@@ -38,6 +38,6 @@ api.add_resource(VistaLogInEmpresa, '/autenticacion/empresas/login')
 jwt = JWTManager(app)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8081))
     app.run(debug=True, host='0.0.0.0', port=port)
     
