d635676772b0b2070443cf98f5a1101ad2e5a938
CharlyMoraUA <ca.morar1@uniandes.edu.co>
2023-10-22 19:14:23 -0500

    EMPRESA

--- empresa/src/app.py
+++ empresa/src/app.py
@@ -23,6 +23,6 @@ api = Api(app)
 api.add_resource(VistaRegistroEmpresa, '/empresa/registro')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8082))
     app.run(debug=True, host='0.0.0.0', port=port)
     
