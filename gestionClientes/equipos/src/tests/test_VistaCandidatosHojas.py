a1a68fe7bc1e33ec8a0b49f073f2f413c9aa5092
CharlyMoraUA <ca.morar1@uniandes.edu.co>
2023-11-18 13:08:19 -0500

    db fix test

--- gestionClientes/equipos/src/tests/test_VistaCandidatosHojas.py
+++ gestionClientes/equipos/src/tests/test_VistaCandidatosHojas.py
@@ -1,75 +1,75 @@
-# from datetime import datetime
-# import json
-# from unittest import TestCase
-# from flask_jwt_extended import create_access_token
-# from app import app, sqlpass, test
-# import mysql.connector
+from datetime import datetime
+import json
+from unittest import TestCase
+from flask_jwt_extended import create_access_token
+from app import app, sqlpass, test
+import mysql.connector
 
-# class TestVistaCandidatosHojas(TestCase):
+class TestVistaCandidatosHojas(TestCase):
 
-#     def setUp(self):
-#         if test:
-#             app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@0.0.0.0:3306/candidatos'
-#             self.connection = mysql.connector.connect(host='0.0.0.0',
-#             database='candidatos',
-#             user='root',
-#             password='root')
-#             self.connection = mysql.connector.connect(host='0.0.0.0',
-#             database='empleados',
-#             user='root',
-#             password='root')
-#             self.connection = mysql.connector.connect(host='0.0.0.0',
-#             database='empresas',
-#             user='root',
-#             password='root')
+    def setUp(self):
+        if test:
+            app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@0.0.0.0:3306/empresas'
+            self.connection = mysql.connector.connect(host='0.0.0.0',
+            database='empresas',
+            user='root',
+            password='root')
+            self.connection = mysql.connector.connect(host='0.0.0.0',
+            database='candidatos',
+            user='root',
+            password='root')
+            self.connection = mysql.connector.connect(host='0.0.0.0',
+            database='empleados',
+            user='root',
+            password='root')
 
-#         else:
-#             self.connection = mysql.connector.connect(host='34.27.118.190',
-#             database='candidatos',
-#             user='root',
-#             password=sqlpass)
+        else:
+            self.connection = mysql.connector.connect(host='34.27.118.190',
+            database='candidatos',
+            user='root',
+            password=sqlpass)
 
     
-#         self.client = app.test_client()
-#         self.token_de_acceso = create_access_token(identity=123)
-#         self.headers ={'Content-Type': 'application/json',
-#                        "Authorization" : "Bearer "+str(self.token_de_acceso)}
+        self.client = app.test_client()
+        self.token_de_acceso = create_access_token(identity=123)
+        self.headers ={'Content-Type': 'application/json',
+                       "Authorization" : "Bearer "+str(self.token_de_acceso)}
         
-#         sql = "DELETE FROM empleados.empleado where id in(10,20)"
-#         cursor = self.connection.cursor()
-#         cursor.execute(sql)
-#         self.connection.commit()
-#         cursor.close()
-#         sql_crear = "INSERT INTO empleados.empleado (id, nombre ) VALUES (%s, %s)"
-#         val = (10, "Nombre1" )
-#         cursor = self.connection.cursor()
-#         cursor.execute(sql_crear, val)
-#         self.connection.commit()
-#         sql_crear = "INSERT INTO empleados.empleado (id, nombre ) VALUES (%s, %s)"
-#         val = (20, "Nombre2" )
-#         cursor = self.connection.cursor()
-#         cursor.execute(sql_crear, val)
-#         self.connection.commit()
+        sql = "DELETE FROM empleados.empleado where id in(10,20)"
+        cursor = self.connection.cursor()
+        cursor.execute(sql)
+        self.connection.commit()
+        cursor.close()
+        sql_crear = "INSERT INTO empleados.empleado (id, nombre ) VALUES (%s, %s)"
+        val = (10, "Nombre1" )
+        cursor = self.connection.cursor()
+        cursor.execute(sql_crear, val)
+        self.connection.commit()
+        sql_crear = "INSERT INTO empleados.empleado (id, nombre ) VALUES (%s, %s)"
+        val = (20, "Nombre2" )
+        cursor = self.connection.cursor()
+        cursor.execute(sql_crear, val)
+        self.connection.commit()
 
-#     def tearDown(self):
-#         sql = "DELETE FROM empleados.empleado where id in(10,20)"
-#         cursor = self.connection.cursor()
-#         cursor.execute(sql)
-#         self.connection.commit()
-#         cursor.close()
+    def tearDown(self):
+        sql = "DELETE FROM empleados.empleado where id in(10,20)"
+        cursor = self.connection.cursor()
+        cursor.execute(sql)
+        self.connection.commit()
+        cursor.close()
 
 
-#     def test_get_candidatos_hojas_success(self):
-#         response = self.client.get('/proyectos/1/hojas-trabajo/1', headers=self.headers)
-#         self.assertEqual(response.status_code, 200)
-#         data = json.loads(response.data)
-#         self.assertEqual(data['status_code'], 200)
-#         self.assertEqual(len(data['candidatos']), 2)
+    def test_get_candidatos_hojas_success(self):
+        response = self.client.get('/proyectos/1/hojas-trabajo/1', headers=self.headers)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertEqual(data['status_code'], 200)
+        self.assertEqual(len(data['candidatos']), 2)
 
-#     def test_get_candidatos_hojas_hoja_not_found(self):
-#         response = self.client.get('/proyectos/1/hojas-trabajo/200', headers=self.headers)
-#         self.assertEqual(response.status_code, 404)
-#         data = json.loads(response.data)
-#         self.assertEqual(data['status_code'], 404)
-#         self.assertEqual(data['message'], 'No se encontró la hoja de trabajo')
+    def test_get_candidatos_hojas_hoja_not_found(self):
+        response = self.client.get('/proyectos/1/hojas-trabajo/200', headers=self.headers)
+        self.assertEqual(response.status_code, 404)
+        data = json.loads(response.data)
+        self.assertEqual(data['status_code'], 404)
+        self.assertEqual(data['message'], 'No se encontró la hoja de trabajo')
 
--- gestionClientes/equipos/src/tests/test_VistaEvaluarCandidato.py
+++ gestionClientes/equipos/src/tests/test_VistaEvaluarCandidato.py
@@ -1,62 +1,62 @@
-# from datetime import datetime
-# import json
-# from unittest import TestCase
-# from flask_jwt_extended import create_access_token
-# from app import app, sqlpass, test
-# import mysql.connector
+from datetime import datetime
+import json
+from unittest import TestCase
+from flask_jwt_extended import create_access_token
+from app import app, sqlpass, test
+import mysql.connector
 
-# class TestVistaEvaluarCandidato(TestCase):
+class TestVistaEvaluarCandidato(TestCase):
 
-#     def setUp(self):
-#         if test:
-#             app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@0.0.0.0:3306/candidatos'
-#             self.connection = mysql.connector.connect(host='0.0.0.0',
-#             database='candidatos',
-#             user='root',
-#             password='root')
-#             self.connection = mysql.connector.connect(host='0.0.0.0',
-#             database='empleados',
-#             user='root',
-#             password='root')
-#             self.connection = mysql.connector.connect(host='0.0.0.0',
-#             database='empresas',
-#             user='root',
-#             password='root')
+    def setUp(self):
+        if test:
+            app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@0.0.0.0:3306/empresas'
+            self.connection = mysql.connector.connect(host='0.0.0.0',
+            database='empresas',
+            user='root',
+            password='root')
+            self.connection = mysql.connector.connect(host='0.0.0.0',
+            database='candidatos',
+            user='root',
+            password='root')
+            self.connection = mysql.connector.connect(host='0.0.0.0',
+            database='empleados',
+            user='root',
+            password='root')
 
-#         else:
-#             self.connection = mysql.connector.connect(host='34.27.118.190',
-#             database='candidatos',
-#             user='root',
-#             password=sqlpass)
+        else:
+            self.connection = mysql.connector.connect(host='34.27.118.190',
+            database='candidatos',
+            user='root',
+            password=sqlpass)
     
-#         self.client = app.test_client()
-#         self.token_de_acceso = create_access_token(identity=123)
-#         self.headers ={'Content-Type': 'application/json',
-#                        "Authorization" : "Bearer "+str(self.token_de_acceso)}
+        self.client = app.test_client()
+        self.token_de_acceso = create_access_token(identity=123)
+        self.headers ={'Content-Type': 'application/json',
+                       "Authorization" : "Bearer "+str(self.token_de_acceso)}
 
-#     # def tearDown(self):
+    # def tearDown(self):
 
 
-#     def test_post_evaluar_candidato_success(self):
-#         data = {
-#             "evaluacion": "Good",
-#             "puntaje": 90
-#         }
-#         response = self.client.post('/proyectos/evaluacion/31', data=json.dumps(data), headers=self.headers)
-#         self.assertEqual(response.status_code, 201)
-#         data = json.loads(response.data)
-#         self.assertEqual(data['status_code'], 201)
-#         self.assertEqual(data['candidatos'], 31)
+    def test_post_evaluar_candidato_success(self):
+        data = {
+            "evaluacion": "Good",
+            "puntaje": 90
+        }
+        response = self.client.post('/proyectos/evaluacion/31', data=json.dumps(data), headers=self.headers)
+        self.assertEqual(response.status_code, 201)
+        data = json.loads(response.data)
+        self.assertEqual(data['status_code'], 201)
+        self.assertEqual(data['candidatos'], 31)
 
-#     def test_post_evaluar_candidato_candidato_not_found(self):
-#         data = {
-#             "evaluacion": "Good",
-#             "puntaje": 90
-#         }
-#         response = self.client.post('/proyectos/evaluacion/200004', data=json.dumps(data), headers=self.headers)
-#         self.assertEqual(response.status_code, 404)
-#         data = json.loads(response.data)
-#         self.assertEqual(data['status_code'], 404)
-#         self.assertEqual(data['message'], 'No se encontró el candidato')
+    def test_post_evaluar_candidato_candidato_not_found(self):
+        data = {
+            "evaluacion": "Good",
+            "puntaje": 90
+        }
+        response = self.client.post('/proyectos/evaluacion/200004', data=json.dumps(data), headers=self.headers)
+        self.assertEqual(response.status_code, 404)
+        data = json.loads(response.data)
+        self.assertEqual(data['status_code'], 404)
+        self.assertEqual(data['message'], 'No se encontró el candidato')
 
 
