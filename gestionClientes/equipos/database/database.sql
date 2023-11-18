b890f00cb81ac032b5fa3fc2daf4b1e8e4612b41
CharlyMoraUA <ca.morar1@uniandes.edu.co>
2023-11-18 10:59:12 -0500

    candidatos db created

--- gestionClientes/equipos/database/database.sql
+++ gestionClientes/equipos/database/database.sql
@@ -1,3 +1,25 @@
+CREATE DATABASE candidatos;
+use candidatos;
+
+ALTER DATABASE candidatos CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE candidato(
+    id int not null AUTO_INCREMENT,
+    tipo_doc varchar(10) NOT NULL,
+    num_doc varchar(50) NOT NULL,
+    nombre varchar(100) NOT NULL,
+    usuario varchar(50) NOT NULL,
+    clave varchar(50) NOT NULL,
+    telefono varchar(30) NOT NULL,
+    email varchar(100) NOT NULL,
+    pais varchar(50) NOT NULL,
+    ciudad varchar(50) NOT NULL,
+    aspiracion_salarial bigint NOT NULL,
+    fecha_nacimiento datetime NOT NULL,
+    idiomas varchar(200) NOT NULL,
+    PRIMARY KEY(id)
+);
+
 CREATE DATABASE empresas;
 use empresas;
 
