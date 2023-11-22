d075d204e903164892ce3eb96ec877d9fd93d0fb
CharlyMoraUA <ca.morar1@uniandes.edu.co>
2023-11-22 12:21:06 -0500

    sql pass udpate

--- candidate/src/app.py
+++ candidate/src/app.py
@@ -11,9 +11,10 @@ from datetime import timedelta
 import os
 
 sqlpass = os.getenv("SQL_PASSWORD")
-app = Flask(__name__)
+rootsqlpass = os.getenv("SQL_ROOT_TEST_PASS")
 test = os.getenv('IF_TEST')
 jwt_secret_key = os.getenv('JWT_SECRET_KEY')
+app = Flask(__name__)
 
 CORS(app, origins=["http://localhost:4200", "http://localhost:4201", "http://localhost:8000", "https://micro-web-kdbo2knypq-uc.a.run.app", "http://localhost", "https://localhost"])
 if(os.path.isdir('/cloudsql/')):
@@ -23,9 +24,9 @@ if(os.path.isdir('/cloudsql/')):
         }
 else:
     if test:
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@0.0.0.0:3306/candidatos'
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{rootsqlpass}@0.0.0.0:3306/candidatos'
         app.config['SQLALCHEMY_BINDS'] = {
-            "empresas": "mysql+pymysql://root:root@0.0.0.0:3306/empresas"
+            "empresas": f'mysql+pymysql://root:{rootsqlpass}@0.0.0.0:3306/empresas'
         }
     else:
         app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{sqlpass}@34.27.118.190:3306/candidatos'
