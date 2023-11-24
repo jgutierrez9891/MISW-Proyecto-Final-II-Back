160e930595ea5ef186e4c36949bfaf9f2b3d1a6c
dchala8 <d.chala@uniandes.edu.co>
2023-11-24 17:47:09 -0500

    fix

--- candidate/src/tests/test_info_laboral.py
+++ candidate/src/tests/test_info_laboral.py
@@ -193,13 +193,4 @@ class TestInfoLaboral(TestCase):
 
     def tearDown(self):
 
-        infosLaborales = db.session.query(infoLaboral).all()
-        for item in infosLaborales:
-            db.session.delete(item)
-        
-        candidatos = db.session.query(candidato).all()
-        for item in candidatos:
-            db.session.delete(item)
-
-        db.session.commit()
         return super().tearDown()
